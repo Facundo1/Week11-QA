47b18494a71719a0483ce9e29c64487508807f0d
Facundo Loberse <facundosa123@gmail.com>
2021-04-14 14:29:42 -0300

    post request in progress...

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+// Importing express library to create server
 const express = require('express')
 const app = express()
+// library to parse the data that come from the front end
+const bodyParser = require("body-parser");
+// Port of the server
 const port = 4000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const user = {
+  email: '',
+  name: '',
+  password: ''
+}
+
+// Endpoints
+app.post('/register', (req, res) => {
+  var userEmail = req.body.email;
+  var userName = req.body.name;
+  var password = req.body.password;
+  console.log("User saved!: /n the user mail is: " + userEmail + " /n Full name:" + userName + " /n password is " + password);
+
+  res.end("yes");
+})
+
+app.put('/login', (req, res) => {
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
