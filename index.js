18b273b5c8539855d65b2eeeba7b1e5b3ddb2bc5
Facundo Loberse <facundosa123@gmail.com>
2021-04-16 13:20:10 -0300

    fixed put and post endpoints

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 // library to parse the data that come from the front end
 const bodyParser = require("body-parser");
+// library to allow cross origin
 const cors = require('cors');
 // Port of the server
 const port = 4000
@@ -11,22 +12,34 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const user = []
-
+const user = {
+  email: '',
+  name: '',
+  password: ''
+}
 
 // Endpoints
 app.post('/register', (req, res) => {
-
-  user.push(req.body.name);
-  user.push(req.body.email);
-  user.push(req.body.password);
-  user.join(", ")
-  res.send(user);
-
+  user.email = req.body.email
+  user.name = req.body.name
+  user.password = req.body.password
+  res.send({ registered: user });
 })
 
 app.put('/login', (req, res) => {
-  console.log(res)
+  const verifyUser = {
+    email: '',
+    password: ''
+  }
+  verifyUser.email = req.body.email
+  verifyUser.password = req.body.password
+
+  if (verifyUser.email === user.email && verifyUser.password == user.password) {
+    res.send({ logged: verifyUser })
+  }
+  else {
+    res.status(404).send({ error: 'Invalid credentials' })
+  }
 })
 
 app.listen(port, () => {
